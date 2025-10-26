fc4e1a76302d81b941d2d73e1406e2fb64820c8f
Lautaro Aliquó <lautaro.aliquo@gmail.com>
2025-10-26 19:04:51 -0300

    Agregamos menú e iniciamos un módulo con funciones auxiliares

---
+++ funciones_auxiliares.py
@@ -0,0 +1,7 @@
+def imprimir_menu():
+    print("Estas son las opciones que ofrece el menú. Seleccione una por favor: ")
+    print("(1) Buscar un país por nombre                   ||") #50
+    print("(2) Filtrar países                              ||")
+    print("(3) Ordenar países                              ||")
+    print("(4) Ver estadísticas                            ||")
+    return None
--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hola mundo")
+import csv
+import unicodedata
+import funciones_auxiliares
+
+paises = {}
+with open("paises_del_mundo_español.csv", "r") as archivo:
+    for fila in archivo:
+        lista_completa = fila.strip().split(",")
+        nombre = lista_completa.pop(0)
+        paises[nombre] = lista_completa
+    
+n = paises.pop("nombre")
+
+print("¡Bienvenido a la base de datos de todos los países del mundo!")
+funciones_auxiliares.imprimir_menu()
+eleccion = input("")
