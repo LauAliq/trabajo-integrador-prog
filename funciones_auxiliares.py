a931896cc8c52941b4cb74ae8837512183504404
Lautaro Aliquó <lautaro.aliquo@gmail.com>
2025-10-26 20:02:56 -0300

    Añadimos caso 1 y funciones auxiliares

--- funciones_auxiliares.py
+++ funciones_auxiliares.py
@@ -1,7 +1,48 @@
+import unicodedata
+
+# Funciones de validación
+
+def solo_letras(texto):
+    return (not texto == "") and (texto.isalpha())
+
+def solo_numeros(texto):
+    return (not texto == "") and (texto.isdigit())
+
+def es_float(texto):
+    if texto == "":
+        return False
+    else:
+        try:
+            float(texto)
+        except:
+            return False
+        return True
+
+# Funciones generales
+
+def normalizar(texto):
+    texto_normalizado = unicodedata.normalize('NFD', texto)
+    texto_sin_tildes = "".join([c for c in texto_normalizado if unicodedata.category(c) != 'Mn'])
+    return texto_sin_tildes.lower()
+
 def imprimir_menu():
     print("Estas son las opciones que ofrece el menú. Seleccione una por favor: ")
-    print("(1) Buscar un país por nombre                   ||") #50
-    print("(2) Filtrar países                              ||")
-    print("(3) Ordenar países                              ||")
-    print("(4) Ver estadísticas                            ||")
+    print("(1) 🔍 Buscar un país por nombre 🔍") #50
+    print("(2) ⚙️       Filtrar países       ⚙️")
+    print("(3) 📈      Ordenar países       📈")
+    print("(4) 📊     Ver estadísticas      📊")
+    print("(0) ❌           Salir           ❌")
     return None
+
+def buscar_pais(nombre, diccionario):
+    lista = []
+    nombre = normalizar(nombre)
+    for clave in diccionario:
+        if nombre in normalizar(clave):
+            lista.append(clave)
+    if lista == []:
+        print("No se encontraron países.")
+    else:
+        print("Esta es la lista de países encontrada: ")
+        for elemento in lista:
+            print(elemento)
--- main.py
+++ main.py
@@ -14,3 +14,26 @@ n = paises.pop("nombre")
 print("¡Bienvenido a la base de datos de todos los países del mundo!")
 funciones_auxiliares.imprimir_menu()
 eleccion = input("")
+while eleccion not in ["4","3","2","1", "0"]:
+    eleccion = input("Por favor, ingrese una elección válida (0, 1, 2, 3, 4): ")
+
+if eleccion == "1":
+    nombre = input("Ingrese su búsqueda: ")
+    while not funciones_auxiliares.solo_letras(nombre):
+        nombre = input("Por favor, ingrese un nombre válido: ")
+
+    funciones_auxiliares.buscar_pais(nombre, paises)
+
+elif eleccion == "2":
+    pass
+
+elif eleccion == "3":
+    pass
+
+elif eleccion == "4":
+    pass
+
+else:
+    pass
+
+
