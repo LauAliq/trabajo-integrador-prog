66d2ece6a023fb38d01b8eb447e7827b65cd41cc
Lautaro Aliquó <lautaro.aliquo@gmail.com>
2025-10-26 23:14:23 -0300

    Añadimos el caso 2, junto con algunas funciones auxiliares

--- funciones_auxiliares.py
+++ funciones_auxiliares.py
@@ -23,7 +23,7 @@ def es_float(texto):
 def normalizar(texto):
     texto_normalizado = unicodedata.normalize('NFD', texto)
     texto_sin_tildes = "".join([c for c in texto_normalizado if unicodedata.category(c) != 'Mn'])
-    return texto_sin_tildes.lower()
+    return texto_sin_tildes.strip().lower()
 
 def imprimir_menu():
     print("Estas son las opciones que ofrece el menú. Seleccione una por favor: ")
@@ -33,6 +33,18 @@ def imprimir_menu():
     print("(4) 📊     Ver estadísticas      📊")
     print("(0) ❌           Salir           ❌")
     return None
+
+def submenu_filtrar():
+    print("¿Según qué desea filtrar su búsqueda?")
+    print("(1) 🌍       Por continente      🌍")
+    print("(2) 👤  Por rango de habitantes  👤")
+    print("(3) 🏝️   Por rango de superficie  🏝️")
+    print("(0) 🔙           Atrás           🔙")
+
+#def submenu_ordenar():
+
+
+#def submenu_estadisticas()
 
 def buscar_pais(nombre, diccionario):
     lista = []
@@ -46,3 +58,39 @@ def buscar_pais(nombre, diccionario):
         print("Esta es la lista de países encontrada: ")
         for elemento in lista:
             print(elemento)
+
+def por_continente(nombre, diccionario):
+    lista = []
+    nombre = normalizar(nombre)
+    for clave in diccionario:
+        if nombre in normalizar(diccionario[clave][2]):
+            lista.append(clave)
+    print(f"Esta es la lista de países de {nombre}: ")
+    for elemento in lista:
+        print(elemento)
+
+def por_habitantes(min, max, diccionario):
+    lista = []
+    for clave in diccionario:
+        if min <= int(diccionario[clave][0]) and max >= int(diccionario[clave][0]):
+            lista.append(clave)
+    if lista == []:
+        print("No se encontraron países.")
+    else:
+        print(f"Esta es la lista de países que tienen menos de {max} habitantes y más de {min} habitantes:")
+        for elemento in lista:
+            print(elemento)
+        
+def por_superficie(min, max, diccionario):
+    lista = []
+    for clave in diccionario:
+        if min <= int(diccionario[clave][1]) and max >= int(diccionario[clave][1]):
+            lista.append(clave)
+    if lista == []:
+        print("No se encontraron países.")
+    else:
+        print(f"Esta es la lista de países que tienen una superficie menor a {max} km² y mayor a {min} km²:")
+        for elemento in lista:
+            print(elemento)
+
+
--- main.py
+++ main.py
@@ -2,12 +2,21 @@ import csv
 import unicodedata
 import funciones_auxiliares
 
+continentes = ["antartida", "america del sur", "america del norte", "africa", "asia", "oceania", "europa"]
 paises = {}
 with open("paises_del_mundo_español.csv", "r") as archivo:
     for fila in archivo:
-        lista_completa = fila.strip().split(",")
-        nombre = lista_completa.pop(0)
-        paises[nombre] = lista_completa
+        if "Tristán de Acuña" not in fila:
+            lista_completa = fila.strip().split(",")
+            nombre = lista_completa.pop(0)
+            paises[nombre] = lista_completa
+        else:
+            lista_provisoria = fila.strip().split(",")
+            lista_completa = []
+            nombre = lista_provisoria[0].strip('"') + "," + lista_provisoria[1].strip('"')
+            for i in range(2, 5):
+                lista_completa.append(lista_provisoria[i])
+            paises[nombre] = lista_completa
     
 n = paises.pop("nombre")
 
@@ -25,7 +34,36 @@ if eleccion == "1":
     funciones_auxiliares.buscar_pais(nombre, paises)
 
 elif eleccion == "2":
-    pass
+    funciones_auxiliares.submenu_filtrar()
+    subeleccion = input("")
+    while subeleccion not in ["3","2","1", "0"]:
+        subeleccion = input("Por favor, ingrese una elección válida (0, 1, 2, 3): ")
+    if subeleccion == "1":
+        continente = input("Ingrese el continente por el cual desea filtrar los países: ")
+        while funciones_auxiliares.normalizar(continente) not in continentes:
+            continente = input("Ingrese un continente válido (Antártida, América del Norte, América del Sur, Asia, África, Oceanía, Europa): ")
+        funciones_auxiliares.por_continente(continente, paises)
+
+    elif subeleccion == "2":
+        min = input("Ingrese la cota inferior para la cantidad de habitantes: ")
+        while not funciones_auxiliares.solo_numeros(min):
+            min = input("Por favor, ingrese un valor válido: ")
+        max = input("Ingrese la cota superior para la cantidad de habitantes: ")
+        while not funciones_auxiliares.solo_numeros(max):
+            max = input("Por favor, ingrese un valor válido: ")
+        funciones_auxiliares.por_habitantes(int(min), int(max), paises)
+        
+    elif subeleccion == "3":
+        min = input("Ingrese la cota inferior para la extensión de superficie en km²: ")
+        while not funciones_auxiliares.solo_numeros(min):
+            min = input("Por favor, ingrese un valor válido: ")
+        max = input("Ingrese la cota superior para la extensión de superficie en km²: ")
+        while not funciones_auxiliares.solo_numeros(max):
+            max = input("Por favor, ingrese un valor válido: ")
+        funciones_auxiliares.por_superficie(int(min), int(max), paises)
+
+    else:
+        print("Volviendo al menú principal...")
 
 elif eleccion == "3":
     pass
